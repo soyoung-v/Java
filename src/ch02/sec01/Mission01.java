505ad069a5a752ecedfde63c684a63d052c66e89
soyoung <ssssy93@icloud.com>
2026-01-14 12:39:37 +0900

    미션

---
+++ src/ch02/sec01/Mission01.java
@@ -0,0 +1,12 @@
+package ch02.sec01;
+
+public class Mission01 {
+    public static void main(String[] args){
+        double d1 = Math.random();
+        System.out.println("d1: " + d1);
+
+        int re = (int)d1*13+5;
+        System.out.println("re: " + re);
+
+    }
+}
